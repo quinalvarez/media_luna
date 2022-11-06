4bddaf867c8ab63f5e5214eb763e098ba4a139d6
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-11-05 22:57:48 -0300

    chore: base proyect

---
+++ src/test/java/com/d3/Media/Luna/MediaLunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3.Media.Luna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
