205cd766fe41b3233b6e6ea794559363b6645fec
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-11-06 08:40:02 -0300

    chore: forgotten class, just change the package

---
+++ src/main/java/com/d3/media_luna/MediaLunaApplication.java
@@ -0,0 +1,13 @@
+package com.d3.media_luna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaLunaApplication.class, args);
+	}
+
+}
